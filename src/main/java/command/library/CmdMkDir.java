c14aa36dcbdf736175ab5b6c515515d762934dcd
Sugiri <girayable@gmail.com>
2018-11-29 16:34:43 +0700

    commit sebelum pulang

--- src/main/java/command/library/CmdMkDir.java
+++ src/main/java/command/library/CmdMkDir.java
@@ -10,6 +10,8 @@ import interfaces.IDrive;
 import interfaces.IOutputter;
 import command.framework.Command;
 import filesystem.Directory;
+import filesystem.FileSystemItem;
+import java.util.ArrayList;
 
 class CmdMkDir extends Command {
     private static final String PARAMETER_CONTAINS_BACKLASH = "At least one parameter denotes a path rather than a directory name.";
@@ -48,10 +50,28 @@ class CmdMkDir extends Command {
     @Override
     public void execute(IOutputter outputter) {
         for(int i=0 ; i<getParameterCount() ; i++)
-        {
-            CreateDirectory(getParameterAt(i), this.getDrive());
+        {   
+            if(checkExists(getParameterAt(i))){
+                System.out.println("The directory has already exists !");
+            } else {
+                CreateDirectory(getParameterAt(i), this.getDrive());
+            }            
         }
     }
+    
+    private boolean checkExists (String filename){
+                ArrayList<FileSystemItem> fileExists = this.getDrive().getCurrentDirectory().getContent();
+                boolean status = false;
+                
+                for(int i =0; i<fileExists.size(); i++){
+                    if(!fileExists.get(i).getName().equals(filename) && status == false && fileExists.get(i).isDirectory()){
+                        status = false;
+                    } else{
+                        status = true;
+                    }
+                }
+                return status;
+        }
 
     private static void CreateDirectory(String newDirectoryName, IDrive drive) {
         Directory newDirectory = new Directory(newDirectoryName);
